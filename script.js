eb05d4155c8a5a0475a834dd9e3994ee8d519591
Welington Manfrin <welington.robertodev@gmail.com>
2023-01-22 19:07:48 -0300

    debug

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form")
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
@@ -10,7 +10,7 @@ function add() {
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-    alert("Dia já incluso ❌")
+    alert("Dia já incluso 🔴")
     return
   }
 
@@ -19,10 +19,9 @@ function add() {
 }
 
 function save() {
-  localStorage.setItem("habitsData", JSON.stringify(nlwSetup.data))
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem("habitsData")) || {}
-
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
