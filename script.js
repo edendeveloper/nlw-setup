9f6c0f5ea30b4b05aaf1fdc1c25920925e6fff16
Welington Manfrin <welington.robertodev@gmail.com>
2023-01-22 18:20:30 -0300

    inital commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  //const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const today = "05/01"
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("habitsData", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("habitsData")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
